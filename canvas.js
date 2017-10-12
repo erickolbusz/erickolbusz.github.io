7d627e44d054b5cfcf70ed9a0a282530220fe1ee
erickolbusz <erickolbusz@gmail.com>
2017-10-12 01:09:30 -0400

    to show someone

--- canvas.js
+++ canvas.js
@@ -5,25 +5,41 @@ canvas.width = window.innerWidth * 0.5;
 canvas.height = window.innerHeight * 0.975;
 var ctx = canvas.getContext("2d");
 
-var H,W,M; //height, width, min of the two
+//height, width, min of the two
+var H,W,M;
 W = canvas.width;
 H = canvas.height;
 (W > H) ? M=H : M=W;
 
+//not typing Math 90000 times
+var cos = Math.cos;
+var sin = Math.sin;
+var pi = Math.PI;
+
 //angles
-var XT = Math.PI/-18;
+var PP = -0.0025;
+var XT = pi/-18;
 var YT = 0;
 var ZT = 0;
 
 //position of box (r=0 g=1 b=2 y=3)
 var ROW = 0;
 var BOXLEVEL = 0;
+var BLACK = "#000000";
+var GRAY = "#aaaaaa";
 var COLORS = ["#db0808", "#08db08", "#086ddb", "#dbdb08"];
+var BORDERS = ["#a00303", "#03a003", "#036da0", "#a0a003"];
+//var BORDERS = ["#ffffff", "#ffffff", "#ffffff", "#ffffff"];
 var NOTES = [];
 
-//not typing Math 90000 times
-var cos = Math.cos;
-var sin = Math.sin;
+//line count for non-rectangular paths and polygons
+var CIRCLE_N = 40;
+var NOTE_CURVE_N = 6;
+
+//animation info
+var ROW_SHIFT_N = 5;
+var ROW_SHIFT_T = 10;
+
 
 //color screen black
 ctx.beginPath();
@@ -65,20 +81,13 @@ var rotZ = function(theta) {
 			[0,0,1,0],
 			[0,0,0,1]];
 }
-//camera matrix
-var camRot = function(cx,cy,cz,ct) {
-	return [[1,0,0,0],
-			[0,cos(ct),-1*sin(ct),0],
-			[0,sin(ct),cos(ct),0],
-			[-1*cx,-1*(cy*cos(ct)+cz*sin(ct)),cy*sin(ct)-cz*cos(ct),1]];
-}
-var camera = camRot(0,-2,-5,Math.PI/18);
 
 
 //PLOTTING ------------------------------------------------------------------------------
 var clearScreen = function() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     console.log("cleared");
+    ctx.fillStyle = BLACK;
     ctx.beginPath();
     ctx.rect(0,0,W,H);
     ctx.fill();
@@ -87,7 +96,6 @@ var clearScreen = function() {
 var plotLine = function(x1, y1, z1,
 						x2, y2, z2,
 						xtheta, ytheta, ztheta) {
-	var r = -0.0025;
 	var rot = matrixMul(matrixMul(rotX(xtheta),rotY(ytheta)),rotZ(ztheta));
 
 	//rotating
@@ -95,40 +103,64 @@ var plotLine = function(x1, y1, z1,
 	var p2 = matrixMul([[x2, y2, z2, 1]],rot);
 
 	//1pp
-	var xa = p1[0][0]/(r*p1[0][2]+1);
-	var ya = p1[0][1]/(r*p1[0][2]+1);
-	var xb = p2[0][0]/(r*p2[0][2]+1);
-	var yb = p2[0][1]/(r*p2[0][2]+1);
+	var xa = p1[0][0]/(PP*p1[0][2]+1);
+	var ya = p1[0][1]/(PP*p1[0][2]+1);
+	var xb = p2[0][0]/(PP*p2[0][2]+1);
+	var yb = p2[0][1]/(PP*p2[0][2]+1);
 
 	//drawing
+	//console.log(xa,ya);
 	ctx.beginPath();
 	ctx.moveTo((0.5+0.01*xa)*W, (0.5+0.01*ya)*H);
 	ctx.lineTo((0.5+0.01*xb)*W, (0.5+0.01*yb)*H);
 	ctx.stroke();
 }
 
-//circle in x-z plane
-var plotCircle = function(cx, cy, cz, r,
-						  xtheta, ytheta, ztheta) {
+//filled circle in x-z plane
+var plotCircleFill = function(cx, cy, cz, r,
+							  xtheta, ytheta, ztheta) {
 	var points = [];
-	var n = 100;
-	for (var i=0; i<n; i++) {
-		var t1 = Math.PI*2*(i-1)/n;
-		var t2 = Math.PI*2*i/n;
-		plotLine(cx+r*cos(t1), cy, cz+r*sin(t1),
-				 cx+r*cos(t2), cy, cz+r*sin(t2),
-				 xtheta, ytheta, ztheta);
+	var rot = matrixMul(matrixMul(rotX(xtheta),rotY(ytheta)),rotZ(ztheta));
+
+	for (var i=0; i<CIRCLE_N; i++) {
+		var t = pi*2*i/CIRCLE_N;
+		var p = matrixMul([[cx+r*cos(t), cy, cz+r*sin(t), 1]], rot);
+		points.push([p[0][0]/(PP*p[0][2]+1), p[0][1]/(PP*p[0][2]+1)]);
 	}
+	ctx.beginPath();
+	ctx.moveTo((0.5+0.01*points[0][0])*W, (0.5+0.01*points[0][1])*H);
+	for (var i=1; i<points.length; i++) {
+		ctx.lineTo((0.5+0.01*points[i][0])*W, (0.5+0.01*points[i][1])*H);
+	}
+	ctx.closePath();
+	ctx.fill();
+	ctx.stroke();
 }
 
-//rectangle
-var plotRect = function(x1, y1, z1,
-						x2, y2, z2,
-						xtheta, ytheta, ztheta) {
-	plotLine(x1, y1, z1,  x1, y1, z2,  xtheta, ytheta, ztheta);
-	plotLine(x1, y1, z2,  x2, y1, z2,  xtheta, ytheta, ztheta);
-	plotLine(x2, y1, z2,  x2, y1, z1,  xtheta, ytheta, ztheta);
-	plotLine(x2, y1, z1,  x1, y1, z1,  xtheta, ytheta, ztheta);
+var plotRectFill = function(x1, y1, z1,
+							x2, y2, z2,
+							xtheta, ytheta, ztheta) {
+	var p1 = [];
+	var rot = matrixMul(matrixMul(rotX(xtheta),rotY(ytheta)),rotZ(ztheta));
+	p1.push(matrixMul([[x1, y1, z1, 1]],rot));
+	p1.push(matrixMul([[x1, y2, z2, 1]],rot));
+	p1.push(matrixMul([[x2, y2, z2, 1]],rot));
+	p1.push(matrixMul([[x2, y1, z1, 1]],rot));
+
+	var p2 = [];
+	for (var i=0; i<p1.length; i++) {
+		p2.push([p1[i][0][0]/(PP*p1[i][0][2]+1), p1[i][0][1]/(PP*p1[i][0][2]+1)])
+	}
+
+	ctx.beginPath();
+	ctx.moveTo((0.5+0.01*p2[0][0])*W, (0.5+0.01*p2[0][1])*H);
+	for (var i=1; i<p2.length; i++) {
+		ctx.lineTo((0.5+0.01*p2[i][0])*W, (0.5+0.01*p2[i][1])*H);
+	}
+	ctx.closePath();
+	ctx.fill();
+	ctx.stroke();
+
 }
 
 //rectangular prism
@@ -153,7 +185,8 @@ var plotRectPrism = function(x1, y1, z1,
 
 //bigbox
 var drawBigBox = function() {
-	ctx.strokeStyle = "#aaaaaa";
+	ctx.strokeStyle = GRAY;
+	ctx.lineWidth = 4;
 	plotRectPrism(-25, 0, -10,
 		 25, 40, 0,
 		 XT, YT, ZT);
@@ -162,6 +195,7 @@ var drawBigBox = function() {
 //small box
 var drawSmallBox = function() {
 	ctx.strokeStyle = COLORS[ROW];
+	ctx.lineWidth = 5;
 	plotRectPrism(-25, BOXLEVEL, -10,
 		 25, BOXLEVEL+10, 0,
 		 XT, YT, ZT);
@@ -169,34 +203,67 @@ var drawSmallBox = function() {
 
 //tracks
 var drawTracks = function() {
-	ctx.strokeStyle = "#aaaaaa";
-    for (var i=-20; i<=20; i+=10) {
+	ctx.strokeStyle = GRAY;
+	ctx.fillStyle = "#111111";
+	ctx.lineWidth = 3;
+	plotRectFill(-20,BOXLEVEL+5,0,20,0,-1000,XT,YT,ZT);
+    for (var i=-10; i<=10; i+=10) {
 		plotLine(i,BOXLEVEL+5,0,i,0,-1000,XT,YT,ZT);
     }
-    plotLine(-20,BOXLEVEL+5,0,20,BOXLEVEL+5,0,XT,YT,ZT);
+    //plotLine(-20,BOXLEVEL+5,0,20,BOXLEVEL+5,0,XT,YT,ZT);
 }
 
 var drawNotes = function() {
 	var note;
 	for (var i=0; i<NOTES.length; i++) {
 		note = NOTES[i];
-		ctx.strokeStyle = COLORS[note.row];
-		//drawing circles on a angled plane,,,,,,
-		//lol pretend that all notes are rectangles for now
-		//z-thickness is 10 i.e. [note.z-5, note.z+5]
-		x1 = 10*note.col-20;
-		x2 = 10*note.col-10;
-		z1 = note.z-5;
-		z2 = note.z+5;
-		y1 = (BOXLEVEL+5)*(1-z1/-1000);
-		y2 = (BOXLEVEL+5)*(1-z2/-1000);
-		plotRect(x1,y1,z1, x2,y2,z2, XT,YT,ZT);
+	    ctx.strokeStyle = BORDERS[note.row];
+	    ctx.fillStyle = COLORS[note.row];
+	    ctx.lineWidth = 3;
+	    var x = 10*note.col-15;
+	    var z = note.z;
+	    var y = BOXLEVEL+5+z*Math.atan((BOXLEVEL+5)/1000);
+
+	    var t = pi/2.75;
+	    var k = 3/4;
+	    plotCircleFill(x, y, z, 5, XT, YT, ZT);
+	    plotCircleFill(x, y-(5*sin(t)*k), z, 5*cos(t), XT, YT, ZT);
+
+	    var rot = matrixMul(matrixMul(rotX(XT),rotY(YT)),rotZ(ZT));
+	    var points = [];
+	    //right arc
+	   	for (var j=0; j<NOTE_CURVE_N; j++) {
+			var dt = j*t/(NOTE_CURVE_N-1);
+			var p = matrixMul([[x+5*cos(dt), y-(5*sin(dt)*k), z, 1]], rot);
+			points.push([p[0][0]/(PP*p[0][2]+1), p[0][1]/(PP*p[0][2]+1)]);
+	    }
+	    ctx.beginPath();
+	    ctx.moveTo((0.5+0.01*points[0][0])*W, (0.5+0.01*points[0][1])*H);
+	    for (var j=1; j<points.length; j++) {
+			ctx.lineTo((0.5+0.01*points[j][0])*W, (0.5+0.01*points[j][1])*H);
+	    }
+	    ctx.stroke();
+
+	    //left arc
+	    points = [];
+	   	for (var j=0; j<NOTE_CURVE_N; j++) {
+			var dt = j*t/(NOTE_CURVE_N-1);
+			var p = matrixMul([[x-5*cos(dt), y-(5*sin(dt)*k), z, 1]], rot);
+			console.log(p);
+			points.push([p[0][0]/(PP*p[0][2]+1), p[0][1]/(PP*p[0][2]+1)]);
+	    }
+	    ctx.beginPath();
+	    ctx.moveTo((0.5+0.01*points[0][0])*W, (0.5+0.01*points[0][1])*H);
+	    for (var j=1; j<points.length; j++) {
+			ctx.lineTo((0.5+0.01*points[j][0])*W, (0.5+0.01*points[j][1])*H);
+	    }
+	    ctx.stroke();
 	}
 }
 
 var drawAll = function() {
     drawTracks();
-	drawNotes();
+    drawNotes();
     drawBigBox();
     drawSmallBox();
 }
@@ -207,16 +274,15 @@ var rowDown = function() {
     if (ROW < 3) {
 		ROW++; //needs to be INSTANT so people don't get screwed by animation	
 		//todo later: rewrite with the fps of the whole program
-		var STEPS = 20;
-		var MS = 20;
 		var anim = setInterval(function() {
 		    clearScreen();
-		    BOXLEVEL += 10/STEPS;
+		    BOXLEVEL += 10/ROW_SHIFT_N;
 		    drawAll();
 		    if (BOXLEVEL - ROW*10 > -0.0001) {
+		    	BOXLEVEL = ROW*10;
 		    	clearInterval(anim);
 		    }
-		},MS/STEPS);
+		},ROW_SHIFT_N/ROW_SHIFT_T);
     }
 }
 
@@ -224,16 +290,17 @@ var rowUp = function() {
     if (ROW > 0) {
 		ROW--; //needs to be INSTANT so people don't get screwed by animation	
 		//todo later: rewrite with the fps of the whole program
-		var STEPS = 20;
+		var STEPS = 10;
 		var MS = 20;
 		var anim = setInterval(function() {
 		    clearScreen();
-		    BOXLEVEL -= 10/STEPS;
+		    BOXLEVEL -= 10/ROW_SHIFT_N;
 		    drawAll();
 		    if (BOXLEVEL - ROW*10 < 0.0001) {
+		    	BOXLEVEL = ROW*10;
 		    	clearInterval(anim);
 		    }
-		},MS/STEPS);
+		},ROW_SHIFT_N/ROW_SHIFT_T);
     }
 }
 
@@ -266,18 +333,7 @@ function keyPressed(e) {
     else if (e.keyCode == 220) {
     	//just testing stuff lol
 		console.log("\\");
-		var STEPS = 360;
-		var MS = 500;
-		ZT = -2*Math.PI;
-		var anim = setInterval(function() {
-		    clearScreen();
-		    ZT += 2*Math.PI/STEPS;
-		    drawAll();
-		    if (ZT > -0.0001) {
-		    	ZT = 0;
-		    	clearInterval(anim);
-		    }
-		},MS/STEPS);
+		createNote(rand(),rand(),-1000);
     }
 }
 
@@ -314,26 +370,31 @@ var createNote = function(r, c, z) {
 	NOTES.push({row:r, col:c, z:z});
 }
 
-for (var i=0; i>=-1000; i-=100) {
-	createNote((i/-100)%4,0,i);
-}
-
 //var n = "1,y,0:01|2,y,0:23|2,g,0:24|4,b,0:40"
 //console.log(parseTrack(n));
 //playTrack(parseTrack(n));
+function rand() {
+    return Math.floor(4*Math.random());
+}
 
 drawBigBox();
 drawTracks();
 drawSmallBox();
 drawNotes();
 
-var a = [[1,0],[0,-3/2]];
-var b = [[1,-2],[0,1]];
-var c = [[1,0],[-1,1]];
-var d = [[1/3,0],[0,1]];
-var e = [[1,0],[2,1]];
+var scroll = setInterval(function() {
+    clearScreen();
+    for (var i=0; i<NOTES.length; i++) {
+    	NOTES[i].z+=3;
+    }
+    drawAll();
+},2);
 
-var f = matrixMul(a,b);
-var g = matrixMul(c,d);
-var h = matrixMul(f,g);
-console.log(f,g,h);
+//plotCircleFill(5,5,0,5,XT,YT,ZT);
+
+
+/*
+
+for (var i=0; i>-1000; i-=100) {
+	plotCircle(5,5,i,5,pi/-20,0,0);
+}*/
