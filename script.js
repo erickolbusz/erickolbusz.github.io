9a77b212a0806cfe923099d8f8eb1c3ff988931a
erickolbusz <erickolbusz@gmail.com>
2017-11-02 20:53:24 -0400

    format display

--- script.js
+++ script.js
@@ -34,23 +34,48 @@ var data = {
 	}
 */
 
+var changeData = function(i) {
+	var s = data[i].name + "<a href=\""+ data[i].url + "\"><img class=\"proj-git\" src=\"GitHub-Mark-120px-plus.png\"></img></a>";
+	document.getElementById("tempName").innerHTML = s;
+	document.getElementById("tempDesc").innerHTML = data[i].desc;
+
+}
 var setup = function(proj) {
-	var projId = proj.id;
-	console.log(projId, data[projId], data[projId].name);
+	//var projId = proj.id;
+	//console.log(projId, data[projId], data[projId].name);
 	proj.onmouseover = function() {
-		console.log("e");
-		var s = data[projId].name + "<a href=\""+ data[projId].url + "\"><img class=\"proj-git\" src=\"GitHub-Mark-120px-plus.png\"></img></a>";
-		document.getElementById("tempName").innerHTML = s;
-		document.getElementById("tempDesc").innerHTML = data[projId].desc;
+		//console.log("e");
+		changeData(proj.id);
 	}
+	/*proj.onclick = function() {
+		//console.log("e");
+		changeData(proj.id);
+	}*/
 }
 
 var table = document.getElementById("projTable");
 var rows = table.children[0].children;
+var firstProj = null;
 for (var i=0; i<rows.length; i++) {
 	var cells = rows[i].children;
 	for (var j=0; j<cells.length; j++) {
 		var proj = cells[j].children[0];
 		setup(proj);
+		if (i==0 && j==0) {
+			firstProj = proj;
+		}
 	}
 }
+
+//default to first project
+changeData(firstProj.id);
+/*
+				<div class="project">
+					<p class="proj-name" id="tempName">
+					</p>
+					<hr class="proj-hr" />
+					<p class="proj-text" id="tempDesc">
+						Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test. Test.
+					</p>
+				</div>*/
+
