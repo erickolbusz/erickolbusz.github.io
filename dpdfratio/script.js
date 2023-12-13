d6cbce686aa09e09d8353bf0769af2b17c46d8dc
erickolbusz <erickolbusz@gmail.com>
2023-12-13 14:25:21 -0500

    add dpdf to pdf ratio plots

---
+++ dpdfratio/script.js
@@ -0,0 +1,17 @@
+var NUMFRAMES = 41;
+var NP = 151;
+var scaleslider = document.getElementById("x2range");
+var dpdfplot = document.getElementById("dpdfplot");
+
+var dpdf_numframe = 0;
+var dpdf_currentframe = 0;
+scaleslider.oninput = function() {
+	//now we need to round the value to the nearest frame
+	//min -> frame 0
+	//max -> frame numframes-1
+	dpdf_numframe = Math.round((NUMFRAMES-1)*(this.value - this.min)/(this.max - this.min));
+	if (dpdf_numframe != dpdf_currentframe) {
+		dpdf_currentframe = dpdf_numframe;
+		dpdfplot.src = `./framedata/${NP}/n_p${NP}_dpdfratioslice${dpdf_currentframe}.png`;
+	}
+}
