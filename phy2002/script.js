ebfb95ee6e6ebbc878eabf91aacaad7c424a129b
erickolbusz <erickolbusz@gmail.com>
2024-12-21 15:13:17 -0500

    add final, update

--- phy2002/script.js
+++ phy2002/script.js
@@ -1,41 +1,41 @@
 const classdata = [
-    {key: "02316554e457242498818e4ec433a79724ff23b4d91a8d005de1edc3c4bdb9c1", val: "6464645f64646464626467828e"}, 
-    {key: "0a1fcbf6ba4fd74f9026b1674e28566f5b5a904ab7980c07d8c7f16a50f151df", val: "6464640064645a626464458c8f"}, 
-    {key: "0c5c637f62dd923a82489555c265451682b6e35d1eee91f8fb18021ba2624a9e", val: "64646400000000000000ffffff"}, 
-    {key: "0c5e8ca0d6342ac078bd432097fa1e42407800ec5a277ba51cbc0d9341bf3612", val: "6464640000005f64646482968b"}, 
-    {key: "0d4af6f17e08419a3f477e0ba3c26e89a8cc73d1ce1e4e5cf319867528dedeab", val: "64646400006158646464045759"}, 
-    {key: "0f21917a91db9738c9f423dc8364832bad5d86b79efcd93740e8b54f0d68eb2e", val: "646464636400006464648c9571"}, 
-    {key: "22f327b1434e7e746ca3fe2bb425d8f1be8b5b851f9b24d6c724755a4f106335", val: "6464646300645f646464597485"}, 
-    {key: "232c1a2b92400c25aedd4692adaf9fbb57f5907b802841db6e8382809a5408e4", val: "6464645a63645f006464514683"}, 
-    {key: "25cdf59a427f8482b7632ccd365ae24ed96322c9ab5d3fe247bc43d87ccb18a5", val: "6464646464645f6464646a8182"}, 
-    {key: "30c048f5dfde42ef9531f62c9619230725e601bc619efabb55be45be1bdde2c7", val: "64640064646400005c64606969"}, 
-    {key: "31bdd0710315b801def3547c13fd762c60719084c68b772666dec0c89060cf86", val: "64625e0064645f64645a478d8e"}, 
-    {key: "36cf3996bc2cb0ff3d85126b9a15a9c9c5690f1fbaeb7460e807cc52f6c64f60", val: "64646463646464646464618d8f"}, 
-    {key: "3b6ca0e604501cb8f36a93e0ff92171d8cf369ea0d4740b33099836246785c27", val: "646200000064000000005b8b95"}, 
-    {key: "3f3f057280abc5d997bfc975ff40a13e73df414676c75a930ac9cbcad07601bd", val: "ff0000636464640064642e394b"}, 
-    {key: "624cbc69d59a1d9335bcd6f762a5cc327c9d93ecbe50efabf5d0efd3bea49a50", val: "6464646264645f640064294586"}, 
-    {key: "665fb06dcf316f5d62a622b907bdeb1a0381aa2ed996f9352c67244b9d4920b6", val: "6464645f006464645c648c9583"}, 
-    {key: "77d69c596e59358620f70b7496d75ee1992990c0364dadc1d54368a7bf477380", val: "6464626000645f6464645d927a"}, 
-    {key: "85270583945ffcba48e5d13848c5f474136e0d97d45dd2ee817fb186d2435bd3", val: "646464640064646464648b9190"}, 
-    {key: "916830e893f846602e034465ebba74988129eab47912c0d9acdf3e7990e3bf16", val: "646464006364640064648c9696"}, 
-    {key: "9c7f348395884d4ad751773e2526e0e85f2052ecbb4437e62c2f213dadf25a39", val: "646464006364006464644b9696"}, 
-    {key: "9d443e72a715b2c96bacbd690cf9f6ba7dbcf76885717ca2f58395c94c32c471", val: "646400636464646462648c9690"}, 
-    {key: "a5de5411ae1ddd399a98589b53c27715d6279389a9cad25aa458dae4df95bb84", val: "6464646264646464646482958b"}, 
-    {key: "ba199a4b5e4516f0b96022a3a8545f69a7f764e334b049005da614e942b5853a", val: "646464645c6455556464656765"}, 
-    {key: "c1d67d2a96c80e27334238412307dcddbe1927fd36a50b2577bca1bd2cb0701a", val: "6464640064645a64625a64848e"},
-    {key: "caa4a26bd05c1d61d532231ca11b95e3514bcf12edb46b1be9df47f8dc4d5c61", val: "6464646464645f6464648c8778"}, 
-    {key: "d26a3b7001a221d257558a57f5067309286fb872510f2e94a3c0faff8182018d", val: "64625e005d505f64645c2d5383"}, 
-    {key: "d4b5c02fd7d2d42e08b89c78a28d2eb8872d35862768b36af62a369ee0d2c63b", val: "6400555e6464646400647b8f91"}, 
-    {key: "d7c1f6ae1702becfeffb7e6b676edff7211dceb9fe3339f6425213a5c587499d", val: "646464646264646464ff2e494c"}, 
-    {key: "db352f8a54dae2336ead1772e85e42bbd4639b08fdbc52b7849be543c9165539", val: "640064005800006400645a6571"}, 
-    {key: "e1b7da8458b0a280655ab5519099f3993ad81b5ade162becbac7159b401b18b7", val: "6464646462645f645a64564373"}, 
-    {key: "e2547c73eb911913257e151b9fae4851cd55431bd46878651f145352a1046ba6", val: "64646461645f6464626489968e"}, 
-    {key: "e5d8d2680f6afed137e4c36b1c14471861ab63d3ae40ea7f2e7c8d66262b3d38", val: "6464646064645f000064719696"}, 
-    {key: "eb1d362c6b11a0f06028aad87f2861827047d8638717cbf0459bd1c4af57930b", val: "646261000000640053003b5b73"}, 
-    {key: "edc2c721d907ccee3ce7854c1fe081e4da0bdd8fcd1bd520e23b28a80834cffb", val: "6464646364645f6462635c626f"}, 
-    {key: "ee8817b63f5b32e5620ce3eb4827f6beb0855526e70f5e8530358452f249b601", val: "64645e5c615a5f64625a497184"}, 
-    {key: "f05724362a22e56988ebeab77cc085b72002f73e229825daf7ad263e287edfdf", val: "64646464635f0064640062928c"}, 
-    {key: "f7928d7beeb233058bff8596f27a6d2b8dca8aa214653a5aa7f10e0200be7363", val: "6464645c64645f6464634d505c"}];
+{key: "02316554e457242498818e4ec433a79724ff23b4d91a8d005de1edc3c4bdb9c1", val: "6464645f646464646264ffe67828e64"}, 
+{key: "0a1fcbf6ba4fd74f9026b1674e28566f5b5a904ab7980c07d8c7f16a50f151df", val: "6464640064645a6264640ee458c8f61"}, 
+{key: "0c5c637f62dd923a82489555c265451682b6e35d1eee91f8fb18021ba2624a9e", val: "64646400000000000000ffeffffffff"}, 
+{key: "0c5e8ca0d6342ac078bd432097fa1e42407800ec5a277ba51cbc0d9341bf3612", val: "6464640000005f64646412c82968b64"}, 
+{key: "0d4af6f17e08419a3f477e0ba3c26e89a8cc73d1ce1e4e5cf319867528dedeab", val: "64646400006158646464ffe04575942"}, 
+{key: "0f21917a91db9738c9f423dc8364832bad5d86b79efcd93740e8b54f0d68eb2e", val: "64646463640000646464ffe8c957162"}, 
+{key: "22f327b1434e7e746ca3fe2bb425d8f1be8b5b851f9b24d6c724755a4f106335", val: "6464646300645f646464ffe59748561"}, 
+{key: "232c1a2b92400c25aedd4692adaf9fbb57f5907b802841db6e8382809a5408e4", val: "6464645a63645f00646412a51468361"}, 
+{key: "25cdf59a427f8482b7632ccd365ae24ed96322c9ab5d3fe247bc43d87ccb18a5", val: "6464646464645f646464ffe6a818251"}, 
+{key: "30c048f5dfde42ef9531f62c9619230725e601bc619efabb55be45be1bdde2c7", val: "64640064646400005c64ffe60696963"}, 
+{key: "31bdd0710315b801def3547c13fd762c60719084c68b772666dec0c89060cf86", val: "64625e0064645f64645affe478d8e5b"}, 
+{key: "36cf3996bc2cb0ff3d85126b9a15a9c9c5690f1fbaeb7460e807cc52f6c64f60", val: "646464636464646464640be618d8f5e"}, 
+{key: "3b6ca0e604501cb8f36a93e0ff92171d8cf369ea0d4740b33099836246785c27", val: "64620000006400000000ffe5b8b9560"}, 
+{key: "3f3f057280abc5d997bfc975ff40a13e73df414676c75a930ac9cbcad07601bd", val: "ff000063646464006464ffe2e394b3d"}, 
+{key: "624cbc69d59a1d9335bcd6f762a5cc327c9d93ecbe50efabf5d0efd3bea49a50", val: "6464646264645f640064ffe2945865e"}, 
+{key: "665fb06dcf316f5d62a622b907bdeb1a0381aa2ed996f9352c67244b9d4920b6", val: "6464645f006464645c64ffe8c958364"}, 
+{key: "77d69c596e59358620f70b7496d75ee1992990c0364dadc1d54368a7bf477380", val: "6464626000645f646464ffe5d927a54"}, 
+{key: "85270583945ffcba48e5d13848c5f474136e0d97d45dd2ee817fb186d2435bd3", val: "64646464006464646464ffe8b9190ff"}, 
+{key: "916830e893f846602e034465ebba74988129eab47912c0d9acdf3e7990e3bf16", val: "64646400636464006464ffe8c96965c"}, 
+{key: "9c7f348395884d4ad751773e2526e0e85f2052ecbb4437e62c2f213dadf25a39", val: "64646400636400646464ffe4b96965f"}, 
+{key: "9d443e72a715b2c96bacbd690cf9f6ba7dbcf76885717ca2f58395c94c32c471", val: "64640063646464646264ffe8c969061"}, 
+{key: "a5de5411ae1ddd399a98589b53c27715d6279389a9cad25aa458dae4df95bb84", val: "64646462646464646464ffe82958b64"}, 
+{key: "ba199a4b5e4516f0b96022a3a8545f69a7f764e334b049005da614e942b5853a", val: "646464645c6455556464ffe6567655b"}, 
+{key: "c1d67d2a96c80e27334238412307dcddbe1927fd36a50b2577bca1bd2cb0701a", val: "6464640064645a64625affe64848e60"}, 
+{key: "caa4a26bd05c1d61d532231ca11b95e3514bcf12edb46b1be9df47f8dc4d5c61", val: "6464646464645f646464ffe8c877842"}, 
+{key: "d26a3b7001a221d257558a57f5067309286fb872510f2e94a3c0faff8182018d", val: "64625e005d505f64645cffe2d538363"}, 
+{key: "d4b5c02fd7d2d42e08b89c78a28d2eb8872d35862768b36af62a369ee0d2c63b", val: "6400555e646464640064ffe7b8f9162"}, 
+{key: "d7c1f6ae1702becfeffb7e6b676edff7211dceb9fe3339f6425213a5c587499d", val: "64646464626464646464ffe2e494c34"}, 
+{key: "db352f8a54dae2336ead1772e85e42bbd4639b08fdbc52b7849be543c9165539", val: "64006400580000640064ffe5a657152"}, 
+{key: "e1b7da8458b0a280655ab5519099f3993ad81b5ade162becbac7159b401b18b7", val: "6464646462645f645a64ffe5643734f"}, 
+{key: "e2547c73eb911913257e151b9fae4851cd55431bd46878651f145352a1046ba6", val: "64646461645f64646264ffe89968e61"}, 
+{key: "e5d8d2680f6afed137e4c36b1c14471861ab63d3ae40ea7f2e7c8d66262b3d38", val: "6464646064645f00006412c71969664"}, 
+{key: "eb1d362c6b11a0f06028aad87f2861827047d8638717cbf0459bd1c4af57930b", val: "64626100000064005300ffe3b5b734e"}, 
+{key: "edc2c721d907ccee3ce7854c1fe081e4da0bdd8fcd1bd520e23b28a80834cffb", val: "6464646364645f646263ffe5c626f48"}, 
+{key: "ee8817b63f5b32e5620ce3eb4827f6beb0855526e70f5e8530358452f249b601", val: "64645e5c615a5f64625affe49718459"}, 
+{key: "f05724362a22e56988ebeab77cc085b72002f73e229825daf7ad263e287edfdf", val: "64646464635f00646400ffe62928c64"}, 
+{key: "f7928d7beeb233058bff8596f27a6d2b8dca8aa214653a5aa7f10e0200be7363", val: "6464645c64645f646463ffe4d505c62"}];
 
 //https://stackoverflow.com/questions/14733374/how-to-generate-an-md5-hash-from-a-string-in-javascript-node-js
 async function sha256(message) {
@@ -57,9 +57,31 @@ sha256('abc').then(hash => console.log(hash));
 
 function getgrades(r) {
     let g = [];
-    for (let i=0; i<13; i++) { g.push( parseInt(r.val.slice(2*i,2*(i+1)),16) ); }
-    for (let i=0; i<13; i++) { if (g[i]===255) { g[i] = "Missing";}}
+    for (let i=0; i<10; i++) { g.push( parseInt(r.val.slice(2*i,2*(i+1)),16) ); }
+    g.push( parseInt(r.val.slice(20,23),16) ); //hw 11 :/ oops
+    for (let i=11; i<15; i++) { g.push( parseInt(r.val.slice(2*i+1,2*(i+1)+1),16) ); }
+    for (let i=0; i<15; i++) { 
+        if (g[i]===255 || g[i]===4095) { g[i] = "Missing"; } //legitimately missing
+        if (g[i]===254 || g[i]===4094) { g[i] = "--"; } //extra hw, optional
+    }
     return g;
+}
+
+function numtolet(g) {
+    if (g >= 93) { return "A"; }
+    if (g >= 89) { return "A-"; }
+
+    if (g >= 87) { return "B+"; }
+    if (g >= 84) { return "B"; }
+    if (g >= 81) { return "B-"; }
+
+    if (g >= 77) { return "C+"; }
+    if (g >= 74) { return "C"; }
+    if (g >= 70) { return "C-"; }
+
+    if (g >= 60) { return "D"; }
+
+    return "F";
 }
 
 document.getElementById("button").onclick = function() {
@@ -80,17 +102,19 @@ document.getElementById("button").onclick = function() {
         }
         else {
             let alldata = getgrades(jsb);
+            console.log(alldata);
 
             let sumhw = 0;
-            for (i=0; i<10; i++) { if(alldata[i] !== "Missing") {sumhw+=alldata[i];} }
+            for (i=0; i<11; i++) { if(alldata[i] !== "Missing" && alldata[i] !== "--") {sumhw+=alldata[i];} }
 
-            let es = [[1,alldata[10]], [2,alldata[11]], [3,alldata[12]]];
+            let es = [[1,alldata[11]], [2,alldata[12]], [3,alldata[13]]];
             es.sort(function (a,b) { return a[1]-b[1]; });
 
             let tothw = 0.3*sumhw/10;
             let tote = 0.1*es[0][1] + 0.15*es[1][1] + 0.2*es[2][1];
+            let totf = 0.25*alldata[14];
 
-            outtext = `<b>Homework (30%)</b><br>`+
+            let outtext1 = `<b>Homework (30%)</b><br>`+
                     `&nbsp;&nbsp;&nbsp;&nbsp;HW1: ${alldata[0]}<br>` +
                     `&nbsp;&nbsp;&nbsp;&nbsp;HW2: ${alldata[1]}<br>` +
                     `&nbsp;&nbsp;&nbsp;&nbsp;HW3: ${alldata[2]}<br>` +
@@ -101,8 +125,9 @@ document.getElementById("button").onclick = function() {
                     `&nbsp;&nbsp;&nbsp;&nbsp;HW8: ${alldata[7]}<br>` +
                     `&nbsp;&nbsp;&nbsp;&nbsp;HW9: ${alldata[8]}<br>` +
                     `&nbsp;&nbsp;&nbsp;&nbsp;HW10: ${alldata[9]}<br>` +
+                    `&nbsp;&nbsp;&nbsp;&nbsp;HW11: ${alldata[10]}<br>` +
                     `Average ${sumhw/10}</b><br>` +
-                    `<b>Total Homework Grade: ${tothw.toFixed(2)}/30</b><br>` +
+                    `<b>Weighted Homework Grade: ${tothw.toFixed(2)}/30</b><br>` +
                     `<br><br>` + 
                     `<b>Lowest Exam (10%)</b><br>` + 
                     `&nbsp;&nbsp;&nbsp;&nbsp;Exam ${es[0][0]}: ${es[0][1]}<br>` +
@@ -113,25 +138,24 @@ document.getElementById("button").onclick = function() {
                     `<b>Highest Exam (20%)</b><br>` + 
                     `&nbsp;&nbsp;&nbsp;&nbsp;Exam ${es[2][0]}: ${es[2][1]}<br>` +
                     `<br>` + 
-                    `<b>Total Exam Grade: ${tote.toFixed(2)}/45</b><br>` + 
+                    `<b>Weighted Exam Grade: ${tote.toFixed(2)}/45</b><br>` + 
                     `<br><br>` + 
                     `<b>Final Exam (25%)</b><br>` + 
-                    `&nbsp;&nbsp;&nbsp;&nbsp;TBD<br>` +
-                    `<br><br>` +
-                    `<b>Total Current Grade: ${(tothw+tote).toFixed(2)}/75</b><br>` +
-                    `<br>` +
-                    `Final Grade assuming nothing missing is made up and a grade of...<br>` + 
-                    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;0% on final: &nbsp;&nbsp; ${(tothw+tote).toFixed(2)}/100<br>` + 
-                    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10% on final: &nbsp;&nbsp; ${(tothw+tote+0.25*10).toFixed(2)}/100<br>` + 
-                    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;20% on final: &nbsp;&nbsp; ${(tothw+tote+0.25*20).toFixed(2)}/100<br>` + 
-                    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;30% on final: &nbsp;&nbsp; ${(tothw+tote+0.25*30).toFixed(2)}/100<br>` + 
-                    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;40% on final: &nbsp;&nbsp; ${(tothw+tote+0.25*40).toFixed(2)}/100<br>` + 
-                    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;50% on final: &nbsp;&nbsp; ${(tothw+tote+0.25*50).toFixed(2)}/100<br>` + 
-                    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;60% on final: &nbsp;&nbsp; ${(tothw+tote+0.25*60).toFixed(2)}/100<br>` + 
-                    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;70% on final: &nbsp;&nbsp; ${(tothw+tote+0.25*70).toFixed(2)}/100<br>` + 
-                    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;80% on final: &nbsp;&nbsp; ${(tothw+tote+0.25*80).toFixed(2)}/100<br>` + 
-                    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;90% on final: &nbsp;&nbsp; ${(tothw+tote+0.25*90).toFixed(2)}/100<br>` + 
-                    `&nbsp;&nbsp;&nbsp;&nbsp;100% on final: &nbsp;&nbsp; ${(tothw+tote+0.25*100).toFixed(2)}/100`;
+                    `&nbsp;&nbsp;&nbsp;&nbsp; Grade: ${alldata[14]}<br>` +
+                    `<b>Weighted Final Exam Grade: ${totf.toFixed(2)}/25</b><br>` + 
+                    `<br><br><br>` +
+                    `<b>Overall Grade: ${(tothw + tote + totf).toFixed(2)}/100</b><br>` +
+                    `<b>Letter Grade: ${numtolet(tothw + tote + totf)}</b><br>`;
+
+            let outtext2 = ``;
+            if (alldata[10] === "--") { //hasnt done 
+                outtext2 = `<br><br>` +
+                    `If you do the optional HW11 for all 300 points...<br>` +
+                    `<b>Overall Grade: ${(tothw+9 + tote + totf).toFixed(2)}/100</b><br>` +
+                    `<b>Letter Grade: ${numtolet(tothw+9 + tote + totf)}</b><br>`;
+            }
+
+            outtext = outtext1 + outtext2;
         }
 
         document.getElementById("output").innerHTML = outtext;
